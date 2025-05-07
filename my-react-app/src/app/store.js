d9152361f1ed0d6027107b87cd35edce2067842d
Anju01111 <aanjalipouwath24@gmail.com>
2025-05-07 12:29:45 +0530

    Add backend code

--- my-react-app/src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import UserSlice from '../features/counter/userSlice'
-export const store = configureStore({
-  reducer: {
-    user:UserSlice,
-  },
-})
